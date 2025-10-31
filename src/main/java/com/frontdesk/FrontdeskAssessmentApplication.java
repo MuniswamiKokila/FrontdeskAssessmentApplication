1ff9d0ad1575fed5a528d0379c3b1885ee24f591
Muniswami Kokila <muniswamikokila3@gmail.com>
2025-11-01 00:23:55 +0530

    Added bean for rest template

--- src/main/java/com/frontdesk/FrontdeskAssessmentApplication.java
+++ src/main/java/com/frontdesk/FrontdeskAssessmentApplication.java
@@ -2,10 +2,17 @@ package com.frontdesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FrontdeskAssessmentApplication {
     public static void main(String[] args) {
         SpringApplication.run(FrontdeskAssessmentApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
